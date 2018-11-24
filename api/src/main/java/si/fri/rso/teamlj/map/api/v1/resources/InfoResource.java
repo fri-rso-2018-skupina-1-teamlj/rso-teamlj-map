a404e33522e7803d6abe186e424913a7367e6195
ladislavskufca <ladislavskufca@users.noreply.github.com>
2018-11-24 23:49:28 +0100

    health resource

---
+++ api/src/main/java/si/fri/rso/teamlj/map/api/v1/resources/InfoResource.java
@@ -0,0 +1,59 @@
+package si.fri.rso.teamlj.map.api.v1.resources;
+
+import com.kumuluz.ee.common.runtime.EeRuntime;
+
+import si.fri.rso.teamlj.map.services.configuration.AppProperties;
+import si.fri.rso.teamlj.map.api.v1.dtos.HealthDto;
+import si.fri.rso.teamlj.map.api.v1.dtos.LoadDto;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
+
+@ApplicationScoped
+@Path("/info")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class InfoResource {
+
+    private Logger log = Logger.getLogger(InfoResource.class.getName());
+
+    @Inject
+    private AppProperties appProperties;
+
+    @GET
+    @Path("instanceid")
+    public Response getInstanceId() {
+        String instanceId =
+                "{\"instanceId\" : \"" + EeRuntime.getInstance().getInstanceId() + "\"}";
+        return Response.ok(instanceId).build();
+    }
+
+    @POST
+    @Path("healthy")
+    public Response setHealth(HealthDto health) {
+        appProperties.setHealthy(health.getHealthy());
+        log.info("Setting health to " + health.getHealthy());
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("load")
+    public Response loadOrder(LoadDto loadDto) {
+        for (int i = 1; i <= loadDto.getN(); i++) {
+            fibonacci(i);
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    private long fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
